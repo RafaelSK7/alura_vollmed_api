fa29a03b2bc3ecc3c015cbfe4f61f48a778f9adc
Rafael Kiss <rafaelskiss1@hotmail.com>
2023-02-07 16:49:02 -0300

    Adição do Paciente, com todas funcionalidades

--- api/src/main/java/med/voll/api/medico/DadosListagemMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosListagemMedico.java
@@ -5,5 +5,4 @@ public record DadosListagemMedico(Long id,String nome, String email, String crm,
     public DadosListagemMedico(Medico medico){
         this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
-
 }
---
+++ api/src/main/java/med/voll/api/paciente/PacienteRepository.java
@@ -0,0 +1,10 @@
+package med.voll.api.paciente;
+
+import med.voll.api.medico.Medico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
+}

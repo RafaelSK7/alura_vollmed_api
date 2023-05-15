eaa26aba866bc18cd817fa6f8857cf23ad34a9f5
Rafael Serino Kiss <rafaelskiss1@hotmail.com>
2023-05-15 17:33:37 -0300

    Validações de Integridade

--- api/src/main/java/med/voll/api/domain/consulta/ConsultaService.java
+++ api/src/main/java/med/voll/api/domain/consulta/ConsultaService.java
@@ -1,6 +1,8 @@
 package med.voll.api.domain.consulta;
 
 
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +22,23 @@ public class ConsultaService {
 
     public void agendar(DadosAgendamentoConsulta dados){
 
-        var medico = medicoRepository.findById(dados.idMedico()).get();
+        if(!pacienteRepository.existsById(dados.idPaciente())){
+            throw new ValidacaoException("Id do paciente informado não existe!");
+        }
+
+        if(dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())){
+            throw new ValidacaoException("Id do médico informado não existe!");
+        }
+
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+        var medico = escolherMédico(dados);
         var consulta = new Consulta(null, medico, paciente, dados.data());
         consultaRepository.save(consulta);
+
+    }
+
+    private Medico escolherMédico(DadosAgendamentoConsulta dados) {
+
 
     }
 
